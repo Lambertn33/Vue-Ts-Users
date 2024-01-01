8478225993a736c1d1b164a6b3c3234a35458dfc
Lambertn33 <lambertgithub@gmail.com>
2024-01-01 14:56:30 +0200

    feat: add user additional info

---
+++ src/components/users/TheUser.vue
@@ -0,0 +1,33 @@
+<template>
+    <fwb-card href="#" class="m-2">
+        <div class="p-5">
+            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {{ user.name }} - {{ user.username }}
+            </h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            </p>
+            <div class="mt-8 px-2 flex flex-col gap-3">
+                <div class="flex items-center gap-2">
+                    <v-icon name="ri-mail-fill" />
+                    <span class="text-sm font-semibold">{{ user.email }}</span>
+                </div>
+                <div class="flex items-center gap-2">
+                    <v-icon name="la-city-solid" />
+                    <span class="text-sm font-semibold">{{ user.address.city }} - {{ user.address.street }}</span>
+                </div>
+            </div>
+        </div>
+    </fwb-card>
+</template>
+
+<script setup lang="ts">
+import { type UserInterface } from '@/interfaces';
+
+import { FwbCard } from 'flowbite-vue';
+
+interface UserComponentProps {
+    user: UserInterface
+}
+defineProps<UserComponentProps>();
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+
+import { RiMailFill, LaCitySolid } from "oh-vue-icons/icons";
+
 import App from './App.vue'
+
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+
+addIcons(RiMailFill, LaCitySolid);
+
+app.component("v-icon", OhVueIcon);
 
 app.mount('#app')
